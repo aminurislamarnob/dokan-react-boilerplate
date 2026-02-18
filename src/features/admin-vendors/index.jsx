d85435baed3565946028284a2edaf8acfe9c5708
Md. Aminur Islam <eng.aminurislam@gmail.com>
2026-02-18 18:32:22 +0600

    Add new field to the vendor add/edit form.

--- src/features/admin-vendors/index.jsx
+++ src/features/admin-vendors/index.jsx
@@ -1,7 +1,41 @@
 import domReady from '@wordpress/dom-ready';
 import { __ } from '@wordpress/i18n';
 import { addFilter } from '@wordpress/hooks';
+import { registerPlugin } from '@wordpress/plugins';
+import { Fill } from '@wordpress/components';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { SimpleInput } from '@getdokan/dokan-ui';
 import './admin-vendors.scss';
+
+const VENDOR_STORE = 'dokan/vendors';
+
+const PassportNumberField = () => {
+    const vendor = useSelect(
+        ( select ) => select( VENDOR_STORE ).getCreateOrEditVendor(),
+        []
+    );
+    const { setCreateOrEditVendor } = useDispatch( VENDOR_STORE );
+
+    return (
+        <div>
+            <SimpleInput
+                label={ __( 'Passport Number', 'dokan-react-boilerplate' ) }
+                value={ vendor?.passport_number ?? '' }
+                onChange={ ( e ) => {
+                    setCreateOrEditVendor( {
+                        ...vendor,
+                        passport_number: e.target.value,
+                    } );
+                } }
+                input={ {
+                    placeholder: __( 'Enter passport number', 'dokan-react-boilerplate' ),
+                    id: 'passport-number',
+                    type: 'text',
+                } }
+            />
+        </div>
+    );
+};
 
 domReady( () => {
     addFilter(
@@ -64,4 +98,22 @@ domReady( () => {
             return view;
         }
     );
+
+    registerPlugin( 'dokan-react-boilerplate-vendor-passport-create', {
+        render: () => (
+            <Fill name="dokan-create-new-vendor-after-phone-store-information">
+                <PassportNumberField />
+            </Fill>
+        ),
+        scope: 'dokan-admin-dashboard-vendor-form-dokan-create-new-vendor',
+    } );
+
+    registerPlugin( 'dokan-react-boilerplate-vendor-passport-edit', {
+        render: () => (
+            <Fill name="dokan-edit-vendor-after-phone-store-information">
+                <PassportNumberField />
+            </Fill>
+        ),
+        scope: 'dokan-admin-dashboard-vendor-form-dokan-edit-vendor',
+    } );
 } );
